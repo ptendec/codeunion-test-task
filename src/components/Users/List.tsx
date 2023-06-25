49ee59ed6ebc37662aa126bd02879b1cfcced94a
ptendec <sherkhandeus@gmail.com>
2023-06-26 01:17:05 +0600

    feat: implement search

--- src/components/Users/List.tsx
+++ src/components/Users/List.tsx
@@ -1,4 +1,6 @@
+import { useEffect, useState } from 'react'
 import noAvatar from '../../assets/no_avatar.png'
+import { useDebounce } from '../../hooks/useDebounce'
 import { User } from '../../interfaces'
 import { UserDropdown } from './Dropdown'
 
@@ -15,13 +17,29 @@ export const UsersList = ({
   showAddModal,
   showEditModal,
 }: Props) => {
-  console.log(users[users.length - 1].image || noAvatar)
+  const [search, setSearch] = useState('')
+  const [foundUsers, setFoundUsers] = useState<User[]>(users)
+
+  const debouncedSearch = useDebounce<string>(search, 500)
+
+  useEffect(() => {
+    if (!debouncedSearch) {
+      setFoundUsers(users)
+    } else {
+      const filteredUsers = users.filter(user =>
+        user.email.toLowerCase().includes(debouncedSearch.toLowerCase()),
+      )
+      setFoundUsers(filteredUsers)
+    }
+  }, [debouncedSearch, users])
 
   return (
     <div className='my-4 bg-white p-4 rounded-xl'>
       <div className='flex justify-between mb-8'>
         <p className='text-xl font-semibold'>Команда</p>
         <input
+          value={search}
+          onChange={event => setSearch(event.target.value)}
           type='text'
           placeholder='Поиск по email'
           className='border rounded-xl w-64 px-3 py-1 outline-none text-sm'
@@ -34,37 +52,41 @@ export const UsersList = ({
         </button>
       </div>
       <div>
-        {users.map(user => (
-          <div key={user.email} className='flex gap-x-2 mb-2 relative'>
-            <img
-              src={user.image || noAvatar}
-              className='w-16 h-16 rounded-full object-cover'
-              alt='User avatar'
-            />
-            <div>
-              <div className='flex gap-x-2 mb-2 items-center'>
-                <p>{user.name}</p>
-                <p className='text-gray-500 text-sm font-semibold'>
-                  {user.email}
-                </p>
-              </div>
-              {user.permissions.map(permission => (
-                <span
-                  key={`${user.email}-${permission}`}
-                  className='rounded-xl border-2 px-2 py-0.5 text-sm font-medium mr-2 text-gray-500'
-                >
-                  {permission}
-                </span>
-              ))}
-            </div>
-            <div className='absolute right-0 top-0'>
-              <UserDropdown
-                onEditUser={() => showEditModal(user)}
-                onDeleteUser={() => onDeleteUser(user.email)}
+        {!foundUsers ? (
+          <p>Ничего не найдено</p>
+        ) : (
+          foundUsers.map(user => (
+            <div key={user.email} className='flex gap-x-2 mb-2 relative'>
+              <img
+                src={user.image || noAvatar}
+                className='w-16 h-16 rounded-full object-cover'
+                alt='User avatar'
               />
+              <div>
+                <div className='flex gap-x-2 mb-2 items-center'>
+                  <p>{user.name}</p>
+                  <p className='text-gray-500 text-sm font-semibold'>
+                    {user.email}
+                  </p>
+                </div>
+                {user.permissions.map(permission => (
+                  <span
+                    key={`${user.email}-${permission}`}
+                    className='rounded-xl border-2 px-2 py-0.5 text-sm font-medium mr-2 text-gray-500'
+                  >
+                    {permission}
+                  </span>
+                ))}
+              </div>
+              <div className='absolute right-0 top-0'>
+                <UserDropdown
+                  onEditUser={() => showEditModal(user)}
+                  onDeleteUser={() => onDeleteUser(user.email)}
+                />
+              </div>
             </div>
-          </div>
-        ))}
+          ))
+        )}
       </div>
     </div>
   )
